bda71a8736bd41bb86c75839c758a451eb13c528
shipan3452 <954841234@qq.com>
2017-05-21 16:48:10 +0800

    Update mergeSort.php

--- sort/mergeSort.php
+++ sort/mergeSort.php
@@ -11,35 +11,35 @@ class  MergeSort
 		}
 		$mid=$low+floor(($high-$low)/2);
 	
-	    //将左侧子数组排序
+	        //将左侧子数组排序
 		self::sort($arr,$low,$mid);
 		//将右侧子数组排序
 		self::sort($arr,$mid+1,$high);
 		//归并已排好序的左右数数字
-        return self::_merge($arr,$low,$mid,$high);
+                return self::_merge($arr,$low,$mid,$high);
 	}
-
+ 
 	static private function _merge(&$arr,$low,$mid,$high){
-        $tmp=[];
+                $tmp=[];
 		for ($i=$low; $i <=$high ; $i++) { 
 			$tmp[$i]=$arr[$i];
 		}
 
-        $halfL=$low;
-        $halfR=$mid+1;
+                $halfL=$low;
+                $halfR=$mid+1;
 
-        //从左右子数组中一次取出一个数，比较大小，取较小者，如此继续
-        for ($k=$low; $k <=$high ; $k++) { 
-            if($halfL>$mid){
-			  $arr[$k]=$tmp[$halfR++];
-		    }elseif($halfR>$high){
-		      $arr[$k]=$tmp[$halfL++];
-		    }elseif($tmp[$halfL]<$tmp[$halfR]){
-		      $arr[$k]=$tmp[$halfL++];
-		    }else{
-		      $arr[$k]=$tmp[$halfR++];
-		    }
-        }
+		//从左右子数组中一次取出一个数，比较大小，取较小者，如此继续
+		for ($k=$low; $k <=$high ; $k++) { 
+		    if($halfL>$mid){
+				  $arr[$k]=$tmp[$halfR++];
+			    }elseif($halfR>$high){
+			      $arr[$k]=$tmp[$halfL++];
+			    }elseif($tmp[$halfL]<$tmp[$halfR]){
+			      $arr[$k]=$tmp[$halfL++];
+			    }else{
+			      $arr[$k]=$tmp[$halfR++];
+			    }
+		}
 	}
 }
 
